91d2f35414ac233b5b79128efb0df63834ea8f6e
AlenOrbanic <aorbanic@student.unipu.hr>
2021-10-27 00:23:33 +0200

    dodavanje tablica

--- sustav_za_isplatu_placa.sql
+++ sustav_za_isplatu_placa.sql
@@ -13,6 +13,7 @@ CREATE DATABASE isplata_placa;
      - Elektroničar
      - Tajnica
      - Knjigovođa
+     - Dostavljač
 */
 
 /*
@@ -25,14 +26,31 @@ E: Viša klasa    Audi A6
 F: Luksuzna klasa    Mercedes S-klasa
 S: Sportski automobil
 */
+
+/*
+Dostavljač služi za to kada kupac naruči auto, naš dostavljač odveze auto do njega.
+Dostavljač će imati poseban bonus, za putne troškove.
+*/
+
+/*
+		Vrste plaćanja
+plaćanje poduzećem (za velike firme)
+plaćanje karticom
+plaćanje u kešu
+	
+    moguće je plaćat na rate 
+    d0 60 rata = 5 godina
+*/
+
 CREATE TABLE zaposlenik(
 	id INTEGER NOT NULL PRIMARY KEY,
 	ime VARCHAR(20) NOT NULL,
 	prezime VARCHAR(30) NOT NULL,
-    pozicija VARCHAR(30) NOT NULL,
+    id_pozicija INTEGER NOT NULL,
     oib CHAR(11) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    CHECK (email LIKE "%@%")
+    CHECK (email LIKE "%@%"),
+    FOREIGN KEY (id_pozicija) REFERENCES pozicija(id)
 );
 
 CREATE TABLE satnica(
@@ -61,5 +79,45 @@ CREATE TABLE bonus_sati (
 	rad_vikendom FLOAT DEFAULT 0,
 	nocna_smjena FLOAT DEFAULT 0,
 	rad_blagdanom FLOAT DEFAULT 0,
-    
+    putni_trosak  FLOAT DEFAULT 0
+);
+
+/*
+CREATE TABLE dostava (
+	broj_dostave INTEGER NOT NULL PRIMARY KEY,
+	vrijeme_dostave TIME NOT NULL,
+	id_zaposlenik INTEGER NOT NULL,
+	id_automobil INTEGER NOT NULL,
+	FOREIGN KEY (id_zaposlenik) REFERENCES zaposlenik(id),
+	FOREIGN KEY (id_automobil) REFERENCES automobil(id)
+);
+*/
+
+CREATE TABLE kupac (
+id INTEGER NOT NULL PRIMARY KEY, 
+ime VARCHAR(20) NOT NULL,
+prezime VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+broj_mobitela INTEGER,
+IBAN VARCHAR(50),
+CHECK (email LIKE "%@%")
+);
+
+CREATE TABLE racun (
+	id INTEGER NOT NULL PRIMARY KEY,
+	vrijeme_dostave TIME,
+    vrijeme_izdavanja DATETIME NOT NULL,
+	id_zaposlenik INTEGER NOT NULL,
+	id_automobil INTEGER NOT NULL,
+    vrsta_placanja VARCHAR(30) NOT NULL,
+    id_kupac INTEGER NOT NULL,
+	FOREIGN KEY (id_zaposlenik) REFERENCES zaposlenik(id),
+	FOREIGN KEY (id_automobil) REFERENCES automobil(id),
+    FOREIGN KEY (id_kupac) REFERENCES kupac(id)
+);
+
+CREATE TABLE pozicija (
+	id INTEGER PRIMARY KEY NOT NULL,
+	ime VARCHAR(30),
+    opis TEXT 
 );
