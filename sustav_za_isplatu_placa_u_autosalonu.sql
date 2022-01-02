942ea5c11f7905148302d851cdca180a875214e9
AlenOrbanic <aorbanic@student.unipu.hr>
2022-01-02 14:22:58 +0100

    insertovi, prisutnost i praznici

--- sustav_za_isplatu_placa_u_autosalonu.sql
+++ sustav_za_isplatu_placa_u_autosalonu.sql
@@ -39,10 +39,9 @@ Sta trebamo napraviti:
 
 CREATE TABLE pozicija (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	ime VARCHAR(30) NOT NULL,
-    opis VARCHAR(100) NOT NULL,
-    novac_po_satu INTEGER NOT NULL,
-    UNIQUE (ime, opis)
+	ime VARCHAR(30) NOT NULL UNIQUE,
+    opis VARCHAR(100) NOT NULL UNIQUE,
+    novac_po_satu INTEGER NOT NULL
 );
 
 CREATE TABLE zaposlenik(
@@ -52,8 +51,7 @@ CREATE TABLE zaposlenik(
     id_pozicija INTEGER NOT NULL,
     oib CHAR(11) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
-    datum_zaposlenja DATETIME NOT NULL,
-    UNIQUE (ime, prezime),
+    datum_zaposlenja DATETIME DEFAULT NOW(),
     CONSTRAINT zaposlenik_email_ck CHECK (email LIKE "%@%"),
     CONSTRAINT zaposlenik_pozicija_fk FOREIGN KEY (id_pozicija) REFERENCES pozicija(id)
 );
@@ -131,12 +129,27 @@ CREATE TABLE racun (
     id_automobil INTEGER,
     id_servis INTEGER,
     -- CONSTRAINT racun_datum_izdavanja_ck CHECK (datum_izdavanja < NOW()),
-    CONSTRAINT racun_auto_or_servis_ck CHECK ((id_servis != NULL && id_automobil = NULL) || (id_servis = NULL && id_automobil != NULL)),
+    CONSTRAINT racun_auto_or_servis_ck CHECK ((id_servis != NULL AND id_automobil = NULL) OR (id_servis = NULL AND id_automobil != NULL)),
 	CONSTRAINT racun_zaposlenik_fk FOREIGN KEY (id_zaposlenik) REFERENCES zaposlenik(id),
     CONSTRAINT racun_kupac_fk FOREIGN KEY (id_kupac) REFERENCES kupac(id),
     CONSTRAINT racun_placanje_fk FOREIGN KEY (id_placanje) REFERENCES placanje(id),
     CONSTRAINT racun_automobil_fk FOREIGN KEY (id_automobil) REFERENCES automobil(id),
     CONSTRAINT racun_servis_fk FOREIGN KEY (id_servis) REFERENCES servis(id)
+);
+
+CREATE TABLE praznici (
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+naziv VARCHAR(50) NOT NULL,
+datum VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE prisutnost (
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+id_zaposlenik INTEGER NOT NULL,
+datum DATETIME DEFAULT NOW(),
+broj_sati FLOAT DEFAULT 0,
+broj_sati_sa_bonusima FLOAT DEFAULT 0,
+CONSTRAINT prisutnost_zaposlenik_fk FOREIGN KEY (id_zaposlenik) REFERENCES zaposlenik(id)
 );
 
 INSERT INTO pozicija (ime, opis, novac_po_satu) VALUES
@@ -154,38 +167,38 @@ INSERT INTO pozicija (ime, opis, novac_po_satu) VALUES
 	("Čistać", "Održava radno mjesto čistim", 30),
 	("Čistać Automobila", "Održava automobile u izložbenom stanju", 35);
 
-INSERT INTO zaposlenik (ime, prezime, id_pozicija, oib, email) VALUES
-	("Mlohael", "Ether", 1, "95841550335", "mether@luxecars.com", "2020-04-30 19:21:31"),
-	("Luka", "Dudak", 2, "70125027634", "ldudak@luxecars.com"),
-	("Karlo", "Lugomer", 3, "92670682735", "klugomer@luxecars.com"),
-	("Jessica", "Zoranović", 2, "16096814870", "jzoranovic@luxecars.com"),
-	("Andrej", "Konki", 4, "83871296540", "konkay69420@luxecars.com"),
-	("Branimir", "Horvat", 4, "97236274923", "bhorvat@luxecars.com"),
-	("Stipe", "Stup", 5, "17398368702", "sstup@luxecars.com"),
-	("Velimir", "Kralj", 5, "37610633755", "vkralj@luxecars.com"),
-	("Vladimir", "Jugić", 5, "98345756419", "vjugic@luxecars.com"),
-	("Tihomir", "Tatić", 6, "60064731762", "ttatić@luxecars.com"),
-	("Teuta", "Miškić", 5, "11289379123", "tmiskic@luxecars.com"),
-	("Karolina", "Tunjić", 6, "60896521870", "ktunjić@luxecars.com"),
-	("Mirko", "Barkica", 7, "35675273263", "mbarkica@luxecars.com"),
-	("Vjekoslav", "Vukovoje", 7, "64685408891", "vvukovoje@luxecars.com"),
-	("Goran", "Strujić", 8, "77352474271", "gstrujic@luxecars.com"),
-	("Golub", "Gvozdenko", 8, "56198404250", "ggvozdenko@luxecars.com"),
-	("Mario", "Valjak", 10, "69233667398", "mvaljak@luxecars.com"),
-	("Mladen", "Jantoš", 10, "20761707492", "mjantos@luxecars.com"),
-	("Robert", "Butkić", 10, "07698168587", "rbutkic@luxecars.com"),
-	("Siniša", "Miščević", 10, "23587461757", "smiscevic@luxecars.com"),
-	("Travan", "Dilović", 6, "15865867042", "tdilovic@luxecars.com"),
-	("Jovana", "Kreketić", 6, "89714241090", "jkreketic@luxecars.com"),
-	("Tomas", "Crnić", 9, "65013824596", "tcrnic@luxecars.com"),
-	("Ernjoslav", "Negomir", 9, "33658723917", "enegomir@luxecars.com"),
-	("Alan", "Šibanović", 9, "68956555044", "asibanovic@luxecars.com"),
-	("Tamara", "Galić", 11, "65013824796", "tgalic@luxecars.com"),
-	("Andrijana", "Brzić", 11, "08728711594", "abrzić@luxecars.com"),
-	("Mihael", "Bokser", 12, "11006286754", "mbokser@luxecars.com"),
-	("Jeffery", "Bizos", 12, "87017816387", "jbizos@luxecars.com"),
-	("Orianna", "Ombretta", 13, "07151025941", "oombretta@luxecars.com"),
-	("Danko", "Bananković", 13, "01405821019", "dbanankovic@luxecars.com");
+INSERT INTO zaposlenik (ime, prezime, id_pozicija, oib, email, datum_zaposlenja) VALUES
+	("Mlohael", "Ether", 1, "95841550335", "mether@luxecars.com", "2019-04-30 12:21:31"),
+	("Luka", "Dudak", 2, "70125027634", "ldudak@luxecars.com", "2020-12-30 13:21:51"),
+	("Karlo", "Lugomer", 3, "92670682735", "klugomer@luxecars.com", "2020-06-16 16:21:34"),
+	("Jessica", "Zoranović", 2, "16096814870", "jzoranovic@luxecars.com", "2020-07-10 14:21:38"),
+	("Andrej", "Konki", 4, "83871296540", "konkay69420@luxecars.com", "2020-04-30 15:21:55"),
+	("Branimir", "Horvat", 4, "97236274923", "bhorvat@luxecars.com", "2020-04-20 16:21:44"),
+	("Stipe", "Stup", 5, "17398368702", "sstup@luxecars.com", "2020-12-16 12:21:39"),
+	("Velimir", "Kralj", 5, "37610633755", "vkralj@luxecars.com", "2020-11-30 11:21:32"),
+	("Vladimir", "Jugić", 5, "98345756419", "vjugic@luxecars.com", "2020-09-30 12:21:35"),
+	("Tihomir", "Tatić", 6, "60064731762", "ttatić@luxecars.com", "2020-08-03 13:21:37"),
+	("Teuta", "Miškić", 5, "11289379123", "tmiskic@luxecars.com", "2020-09-10 14:21:36"),
+	("Karolina", "Tunjić", 6, "60896521870", "ktunjić@luxecars.com", "2020-10-30 15:21:30"),
+	("Mirko", "Barkica", 7, "35675273263", "mbarkica@luxecars.com", "2020-11-22 12:21:35"),
+	("Vjekoslav", "Vukovoje", 7, "64685408891", "vvukovoje@luxecars.com", "2020-01-30 13:21:34"),
+	("Goran", "Strujić", 8, "77352474271", "gstrujic@luxecars.com", "2020-02-08 11:21:20"),
+	("Golub", "Gvozdenko", 8, "56198404250", "ggvozdenko@luxecars.com", "2020-04-09 12:21:19"),
+	("Mario", "Valjak", 10, "69233667398", "mvaljak@luxecars.com", "2020-04-10 14:21:19"),
+	("Mladen", "Jantoš", 10, "20761707492", "mjantos@luxecars.com", "2020-03-11 16:21:18"),
+	("Robert", "Butkić", 10, "07698168587", "rbutkic@luxecars.com", "2020-08-12 14:21:17"),
+	("Siniša", "Miščević", 10, "23587461757", "smiscevic@luxecars.com", "2020-10-17 11:21:16"),
+	("Travan", "Dilović", 6, "15865867042", "tdilovic@luxecars.com", "2020-05-13 12:21:15"),
+	("Jovana", "Kreketić", 6, "89714241090", "jkreketic@luxecars.com", "2020-05-14 11:21:14"),
+	("Tomas", "Crnić", 9, "65013824596", "tcrnic@luxecars.com", "2020-06-16 13:21:21"),
+	("Ernjoslav", "Negomir", 9, "33658723917", "enegomir@luxecars.com", "2020-02-27 14:21:31"),
+	("Alan", "Šibanović", 9, "68956555044", "asibanovic@luxecars.com", "2020-01-22 13:21:31"),
+	("Tamara", "Galić", 11, "65013824796", "tgalic@luxecars.com", "2020-03-20 12:21:31"),
+	("Andrijana", "Brzić", 11, "08728711594", "abrzić@luxecars.com", "2020-03-10 11:21:31"),
+	("Mihael", "Bokser", 12, "11006286754", "mbokser@luxecars.com", "2020-06-15 10:21:31"),
+	("Jeffery", "Bizos", 12, "87017816387", "jbizos@luxecars.com", "2020-04-14 12:21:31"),
+	("Orianna", "Ombretta", 13, "07151025941", "oombretta@luxecars.com", "2020-08-16 13:21:31"),
+	("Danko", "Bananković", 13, "01405821019", "dbanankovic@luxecars.com", "2020-11-30 11:21:31");
 
 INSERT INTO isplata (id_zaposlenik, broj_sati, bonus) VALUES
 	(1, NULL, NULL),
@@ -328,7 +341,67 @@ INSERT INTO racun (datum_izdavanja, id_zaposlenik, id_kupac, id_placanje, id_aut
 	("2020-03-07 19:41:16", 9, 2, 3, 44, NULL),
 	("2020-08-11 17:35:42", 8, 1, 1, 51, NULL);
     
-    
+INSERT INTO praznici (naziv, datum) VALUES 
+("Bozic", "-12-25"),
+("Nova godina", "-01-01"),
+("Praznik rada", "-05-01"),
+("Tijelovo","-06-16"),
+("Dan svih svetih", "-01-11");
+
+# Zadatak: Okidač nam osigurava da u slučaju ako je zaposlenik radio preko 8 sati u jednome danu, satnica za prekovremene sate mu se nadodaje na satnicu (+50%) 
+DROP TRIGGER IF EXISTS bi_prisutnost;
+DELIMITER //
+CREATE TRIGGER bi_prisutnost
+	BEFORE INSERT ON prisutnost
+	FOR EACH ROW
+BEGIN
+DECLARE bonus FLOAT;
+DECLARE mjesecdan VARCHAR(6);
+SET new.broj_sati_sa_bonusima = new.broj_sati;
+SET bonus = new.broj_sati - 8;
+SELECT CONCAT("-",DATE_FORMAT(new.datum,"%m"),"-", DATE_FORMAT(new.datum,"%d")) INTO mjesecdan;
+IF new.broj_sati > 8 THEN SET new.broj_sati_sa_bonusima = 8 + (bonus * 1.5);
+END IF;
+IF mjesecdan IN (SELECT datum FROM praznici) THEN SET new.broj_sati_sa_bonusima = new.broj_sati_sa_bonusima + (new.broj_sati * 0.5);
+END IF;
+IF DAYNAME(new.datum) = "Sunday" THEN SET new.broj_sati_sa_bonusima = new.broj_sati_sa_bonusima + (new.broj_sati * 0.5);
+END IF;
+END//
+DELIMITER ;
+
+INSERT INTO prisutnost (id_zaposlenik, datum, broj_sati) VALUES 
+(1,  "2019-05-02 12:21:31", 6),
+(2,  "2020-06-03 10:30:21", 8),
+(3,  "2020-06-04 10:20:22", 10),
+(4,  "2020-06-05 10:10:23", 9),
+(5,  "2020-06-06 10:10:26", 5),
+(6,  "2020-10-07 10:50:28", 6),
+(7,  "2020-11-08 10:50:29", 7),
+(8,  "2020-08-09 10:40:21", 8),
+(9,  "2020-12-02 10:30:21", 10),
+(10,  "2020-11-01 10:20:20", 7),
+(11,  "2020-10-01 10:20:20", 5),
+(12,  "2020-07-01 10:10:20", 4),
+(13,  "2020-04-01 10:10:22", 6),
+(14,  "2020-03-01 10:10:23", 7),
+(15,  "2020-02-02 10:40:24", 8),
+(16,  "2020-01-03 10:30:20", 8),
+(17,  "2020-01-04 10:50:26", 8),
+(18,  "2020-01-07 10:50:29", 8),
+(19,  "2020-01-08 10:40:28", 8),
+(20,  "2020-01-02 10:30:25", 8),
+(21,  "2020-02-03 10:20:24", 8),
+(22,  "2020-03-01 10:10:23", 8),
+(23,  "2020-03-06 10:10:22", 10),
+(24,  "2020-03-07 10:20:22", 9),
+(25,  "2020-03-02 10:20:21", 9),
+(26,  "2020-09-03 10:30:22", 9),
+(27,  "2020-03-04 10:40:20", 8),
+(28,  "2020-04-04 10:20:20", 8),
+(29,  "2020-04-05 10:30:20", 8),
+(30,  "2020-05-06 10:10:20", 7);
+
+
     # Zadatak: Funkcija koja vrača satnicu određenog zaposlenika.
 DROP FUNCTION IF EXISTS satnica;
 DELIMITER //
@@ -368,18 +441,17 @@ DELIMITER ;
 # Primjer:
 SELECT broj_racuna(10) FROM DUAL;
 
-
-DROP TRIGGER IF EXISTS
+# Zadatak: Okidač koji nam osigura da datum zaposlenja postane trenutni datum ako pokušamo zaposliti nekoga u budućem vremenu.
+DROP TRIGGER IF EXISTS bi_zaposlenik;
 DELIMITER //
 CREATE TRIGGER bi_zaposlenik
 	BEFORE INSERT ON zaposlenik
 	FOR EACH ROW
 BEGIN
-	IF (SELECT new.datum_zaposlenja FROM zaposlenik) > NOW() THEN
-		SIGNAL SQLSTATE "40000"
-        SET message_text = "Datum zaposlenja je preko današnjeg datuma.";
-	END IF;
+DECLARE datum VARCHAR(500);
+IF new.datum_zaposlenja > NOW() THEN SET new.datum_zaposlenja = NOW();
+END IF;
 END//
 DELIMITER ;
 
-INSERT INTO
+SELECT * FROM prisutnost
